ac2b12e698c3589246682bb45206622e0991f232
Leonildo Filgueira <leonildolf_12@hotmail.com>
2022-03-23 10:25:43 -0300

    atualizando interface

--- src/interfaces/exercicio_fixacacao/modelo/servicos/ServicoDePagamentoOline.java
+++ src/interfaces/exercicio_fixacacao/modelo/servicos/ServicoDePagamentoOline.java
@@ -2,6 +2,6 @@ package interfaces.exercicio_fixacacao.modelo.servicos;
 
 public interface ServicoDePagamentoOline {
 	
-	double taxaDePagamento (double quantia);
+	double taxaDePagamento (double quantia); // não precisa colocar o modificador de acesso, porque em interface presume-se que o método é publico e abstrato.
 	double juros(double quantia, int meses);
 }
